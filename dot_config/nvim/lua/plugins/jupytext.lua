af17d2337740ea286d4eeb87e20a8c9fd27e6332
Abhinav Natarajan <abhinav.v.natarajan@gmail.com>
2023-10-06 17:32:35 +0100

    Modify jupytext default options

--- dot_config/nvim/lua/plugins/jupytext.lua
+++ dot_config/nvim/lua/plugins/jupytext.lua
@@ -2,6 +2,7 @@ return {
 	"goerz/jupytext.vim",
 	-- event = "BufReadCmd *.ipynb",
 	init = function()
-		vim.g.jupytext_fmt = 'py'
+		vim.g.jupytext_fmt = 'py:percent'
+		vim.g.jupytext_to_ipynb_opts = '--to=ipynb --update'
 	end
 }
